370a598a78a779b596772d620fb64a509b2d6139
Joao-Oliveira9 <joaoliveira9@outlook.com>
2024-09-12 21:33:16 -0300

    Update: Conexão do back-end com front-end e inserção em bacno de dados teste

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,54 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sql = require("mysql2");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+/* Quando alguem mandar um post request pra essa url, eu vou ter ação de uma callback function */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.post("/", (req, res) => {
+    const queryInsert = "INSERT INTO clientes(nome) VALUES (?)";
+    console.log("passei por aqui");
+    //res.send("Resposta do servidor");
+    console.log(req.body.name);
+    const arr = [req.body.name];
+    const teste = pool.execute(queryInsert, arr, (err, results) => {
+        if (err) {
+            console.error("Erro ao inserir dados:", err);
+            return;
+        }
+        else {
+            console.log("salve");
+        }
+    });
+    console.log(teste);
+    pool.query("SELECT * FROM clientes ", (err, rows, fields) => {
+        if (err instanceof Error) {
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        /*    console.log(fields); */
+    });
+    /*
+    const [rows, fields] = pool.query(`SELECT * FROM alunos`);
+    console.log(rows);
+    console.log(fields); */
+    res.send("Dado recebido");
+});
+/* escutando na porta 8000 */
+app.listen(port, () => {
+    console.log("Servidor rodando na porta 8000");
+});
+/* Conexão com o banco de dados */
 const pool = sql.createPool({
     host: "localhost",
     user: "root",
     password: "terradelama9",
-    database: "faculdade_db",
+    database: "mydb",
 });
 pool.getConnection((err, connetion) => {
     if (err) {
@@ -16,7 +59,7 @@ pool.getConnection((err, connetion) => {
         connetion.release();
     }
 });
-pool.query("Describe alunos", (err, results) => {
+pool.query("Describe clientes ", (err, results) => {
     if (err) {
         console.log("erro ao tentar consultar o banco de dados");
         return;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
 const sql = require("mysql2");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
-app.get()
+/* Quando alguem mandar um post request pra essa url, eu vou ter ação de uma callback function */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post("/", (req, res) => {
+  const queryInsert = "INSERT INTO clientes(nome) VALUES (?)";
+  console.log("passei por aqui");
+  //res.send("Resposta do servidor");
+  console.log(req.body.name);
+  const arr = [req.body.name];
+  const teste = pool.execute(queryInsert, arr, (err, results) => {
+    if (err) {
+      console.error("Erro ao inserir dados:", err);
+      return;
+    } else {
+      console.log("salve");
+    }
+  });
+
+  pool.query("SELECT * FROM clientes ", (err, rows, fields) => {
+    if (err instanceof Error) {
+      console.log(err);
+      return;
+    }
+    console.log(rows);
+    /*    console.log(fields); */
+  });
+
+  /* 
+  const [rows, fields] = pool.query(`SELECT * FROM alunos`);
+  console.log(rows);
+  console.log(fields); */
+
+  res.send("Dado recebido");
+});
+
+/* escutando na porta 8000 */
+app.listen(port, () => {
+  console.log("Servidor rodando na porta 8000");
+});
 
 /* Conexão com o banco de dados */
 const pool = sql.createPool({
   host: "localhost",
   user: "root",
   password: "terradelama9",
-  database: "faculdade_db",
+  database: "mydb",
 });
 
 pool.getConnection((err, connetion) => {
@@ -23,7 +64,7 @@ pool.getConnection((err, connetion) => {
   }
 });
 
-pool.query("Describe alunos", (err, results) => {
+pool.query("Describe clientes ", (err, results) => {
   if (err) {
     console.log("erro ao tentar consultar o banco de dados");
     return;
